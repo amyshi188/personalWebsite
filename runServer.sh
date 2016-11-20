39ea401cf61ad89ee1854eac8e17afc991d47139
amyshi188 <ds2ca@virginia.edu>
2016-11-20 13:29:13 -0500

    New theme icarus

--- runServer.sh
+++ runServer.sh
@@ -1,1 +1,1 @@
-hugo server  -t vienna --baseURL=http://amyshi.me/ --port=80 --appendPort=false --bind=0.0.0.0 
+hugo server  -t hugo-icarus-theme --baseURL=http://amyshi.me/ --port=80 --appendPort=false --bind=0.0.0.0 
